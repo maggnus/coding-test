7c7c37f42b21d5c8b2de11c127282a02cd28b565
Maksim Korenev <mkorenev@eyeota.com>
2019-03-21 10:31:09 +0800

    Add hackerrank tasks

---
+++ hackerrank/Arrays/Left Rotation/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the rotLeft function below.
+func rotLeft(a []int32, d int32) []int32 {
+	size := int32(len(a))
+	b := make([]int32, size)
+	d = int32(math.Mod(float64(d), float64(size)))
+	for i, n := range a {
+		p1 := int32(i) - d
+		if p1 < 0 {
+			p1 = p1 + int32(size)
+		}
+		b[p1] = n
+	}
+	return b
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nd := strings.Split(readLine(reader), " ")
+
+	nTemp, err := strconv.ParseInt(nd[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	dTemp, err := strconv.ParseInt(nd[1], 10, 64)
+	checkError(err)
+	d := int32(dTemp)
+
+	aTemp := strings.Split(readLine(reader), " ")
+
+	var a []int32
+
+	for i := 0; i < int(n); i++ {
+		aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+		checkError(err)
+		aItem := int32(aItemTemp)
+		a = append(a, aItem)
+	}
+
+	result := rotLeft(a, d)
+
+	for i, resultItem := range result {
+		fmt.Fprintf(writer, "%d", resultItem)
+
+		if i != len(result)-1 {
+			fmt.Fprintf(writer, " ")
+		}
+	}
+
+	fmt.Fprintf(writer, "\n")
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
