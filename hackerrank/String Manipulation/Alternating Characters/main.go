9c6027c1680c8d0736f70c9ae36393f6d571dcfa
Maksim Korenev <mkorenev@eyeota.com>
2019-03-21 22:26:05 +0800

    Some updates

---
+++ hackerrank/String Manipulation/Alternating Characters/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the alternatingCharacters function below.
+func alternatingCharacters(s string) int32 {
+
+	chars := strings.Split(s, "")
+
+	var k int32
+
+	for i := range chars {
+		if i > 0 && chars[i] == chars[i-1] {
+			k = k + 1
+		}
+	}
+
+	return k
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	qTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	q := int32(qTemp)
+
+	for qItr := 0; qItr < int(q); qItr++ {
+		s := readLine(reader)
+
+		result := alternatingCharacters(s)
+
+		fmt.Fprintf(writer, "%d\n", result)
+	}
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ other/Templates/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
+func fillTemplate(s string, m map[string]string) string {
+	for key := range m {
+		reg := "(\\$?<" + key + "\\$?>)"
+		r := regexp.MustCompile(reg)
+		b := r.ReplaceAllFunc([]byte(s), func(k []byte) []byte {
+			t := string(k)
+			if strings.HasPrefix(t, "$<") || strings.HasPrefix(t, "$>") {
+				return k
+			}
+			key = strings.TrimPrefix(t, "<")
+			key = strings.TrimSuffix(key, ">")
+			return []byte(m[key])
+		})
+		s = string(b)
+	}
+	return s
+}
+
+func main() {
+
+	s := "As computers <word1> more and more $<powerful$>, they won’t be substitutes for <word2>: they’ll be complements."
+	m := map[string]string{
+		"word1": "become",
+		"word2": "humans",
+	}
+
+	result := fillTemplate(s, m)
+
+	fmt.Printf("Input: %s\n", s)
+	fmt.Printf("Result: %s\n", result)
+}
